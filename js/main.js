5e4f625ea3d26e07b3c4b9036984288e85397f2a
Ikechukwu Okuguni <ikmannoble@gmail.com>
2021-08-28 16:22:09 +0100

    Day One Javascript class

---
+++ js/main.js
@@ -0,0 +1,60 @@
+let firstname = "John";
+let lastname = "Doe";
+let address = " 34 Olumide strret Anifowoshe, Ikeja Lagos";
+
+let details = ` My name is ${firstname} ${lastname} I live in ${address}`;
+let summery = "My name is " + firstname + " " + lastname + " I live in  "  + address;
+console.log(details);
+console.log(summery);
+let number = 200;
+let numberfour = "2000";
+
+console.log(5 ** 3);
+console.log(++number);
+console.log(number++);
+console.log( number + 10);
+console.log(number += 10);
+console.log(numberfour);
+console.log(numberfour * 1);
+console.log(parseInt(numberfour));
+console.log(Number(numberfour));
+console.log(parseFloat(numberfour ));
+
+let num=  3>6 & 2 !== 2;
+
+let students =["Ikechukwu","Mike","Tolu","Kemi"];
+let food = [];
+food[0] = "Rice";
+food[1]="Egusi";
+food[2]="Yam";
+console.log(food);
+
+
+let department = [
+    {
+        name:"Ikechukwu",
+        sex:"Male",
+        Age:35,
+        hubbi:["sweeming","Reading","Programming"]
+    },
+    {
+        name:"Mike",
+        sex:"Female",
+        Age:65,
+        hubbi:["suffing","gammingg","Programming"]
+    }
+
+];
+console.log(students);
+console.log(students[0]);
+
+
+console.log(department[0].hubbi[2]);
+console.log(department[0].hubbi);
+
+// console.log(students.length);
+
+// console.log(typeof(students));
+
+
+
